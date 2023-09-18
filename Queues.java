267fb69df1b5208558afe02ccbd3afc4e698526a
Hitesh Kumawat <kumawathitesh2000@gmail.com>
2023-09-18 16:14:46 +0530

    .

--- Queues.java
+++ Queues.java
@@ -258,6 +258,30 @@ public class Queues {
         }
     }
 
+    // Interleave two half of queue
+    public static void interLeave(Queue<Integer> q) {
+        Queue<Integer> q1 = new LinkedList<>();
+        int size = q.size();
+        for (int i = 0; i < size / 2; i++) {
+            q1.add(q.remove());
+        }
+        while (!q1.isEmpty()) {
+            q.add(q1.remove());
+            q.add(q.remove());
+        }
+    }
+
+    // Reverse a Queue
+    public static void reverseQueue(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
     public static void main(String[] args) {
         // QueuesUsingArrays qua = new QueuesUsingArrays(5);
         // qua.add(1);
@@ -310,5 +334,24 @@ public class Queues {
         // First non repeating letter in stream of letters
         // String str = "aabccxb";
         // printNonRepeating(str);
+
+        // Interleave two half of queue
+        // Queue<Integer> q = new LinkedList<>();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // q.add(6);
+        // q.add(7);
+        // q.add(8);
+        // q.add(9);
+        // q.add(10);
+        // // interLeave(q);
+        // reverseQueue(q);
+        // while (!q.isEmpty()) {
+        // System.out.print(q.remove() + " ");
+        // }
+
     }
 }
