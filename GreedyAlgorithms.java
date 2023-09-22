451cc691eae41c181253c511234022d0eadb46b9
Hitesh Kumawat <kumawathitesh2000@gmail.com>
2023-09-22 17:36:53 +0530

    22-sep

--- GreedyAlgorithms.java
+++ GreedyAlgorithms.java
@@ -1,11 +1,26 @@
 import java.util.*;
 
 public class GreedyAlgorithms {
+
+    static class Job {
+        int id;
+        int deadline;
+        int profit;
+
+        Job(int id, int deadline, int profit) {
+            this.id = id;
+            this.deadline = deadline;
+            this.profit = profit;
+        }
+    }
+
     public static void main(String[] args) {
+
+        // Activity selection
         int start[] = { 1, 3, 0, 5, 8, 5 };
         int end[] = { 2, 4, 6, 7, 9, 9 };
 
-        // if start is in sorted order ans end is not in sorted order then:
+        // if end is not in sorted order then:
         // int activities[][] = new int[start.length][3];
         // for (int i = 0; i < start.length; i++) {
         // activities[i][0] = i;
@@ -27,29 +42,101 @@ public class GreedyAlgorithms {
 
         System.out.println("Maximum Activity : " + maxActivity);
 
-        int val[] = {60, 100, 120};
-        int weight[] = {10, 20, 30};
+        // Fractional knapsack
+        int val[] = { 60, 100, 120 };
+        int weight[] = { 10, 20, 30 };
         int W = 50;
 
         double ratio[][] = new double[val.length][2];
-        for(int i = 0; i < val.length; i++){
+        for (int i = 0; i < val.length; i++) {
             ratio[i][0] = i;
-            ratio[i][1] = val[i]/weight[i];
+            ratio[i][1] = val[i] / weight[i];
         }
         Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
         int capacity = W;
         int finalVal = 0;
-        for(int i = ratio.length-1; i >= 0; i--){
-            int idx = (int)ratio[i][0];
+        for (int i = ratio.length - 1; i >= 0; i--) {
+            int idx = (int) ratio[i][0];
             if (capacity >= weight[idx]) {
                 finalVal += val[idx];
                 capacity -= weight[idx];
-            }else{
+            } else {
                 finalVal += (ratio[i][1] * capacity);
                 capacity = 0;
                 break;
             }
         }
         System.out.println("Maximum Value : " + finalVal);
+
+        // Minimum absolute difference
+        int A[] = { 1, 2, 3 };
+        int B[] = { 2, 1, 3 };
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+        for (int i = 0; i < A.length; i++) {
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+        System.out.println("MInimum absolute difference : " + minDiff);
+
+        // Maximum length chain pair
+        int pairs[][] = { { 5, 24 }, { 39, 60 }, { 5, 28 }, { 27, 40 }, { 50, 90 } };
+
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+        int chainLength = 1;
+        int chainEnd = pairs[0][1];
+
+        for (int i = 1; i < pairs.length; i++) {
+            if (pairs[i][0] > chainEnd) {
+                chainLength++;
+                chainEnd = pairs[i][1];
+            }
+        }
+        System.out.println("Maximum length chain pair : " + chainLength);
+
+        // Indian coins
+        Integer coins[] = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
+        Arrays.sort(coins, Comparator.reverseOrder());
+        int countOfCoins = 0;
+        int amount = 590;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                while (coins[i] <= amount) {
+                    countOfCoins++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
+            }
+        }
+        System.out.println("Total (min) Coins used : " + countOfCoins);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
+
+        // Job Sequencing Problem
+        int jobsInfo[][] = { { 4, 20 }, { 1, 10 }, { 1, 40 }, { 1, 30 } };
+        ArrayList<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < jobsInfo.length; i++) {
+            jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+        }
+        Collections.sort(jobs, (a, b) -> b.profit - a.profit);
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for (int i = 0; i < jobs.size(); i++) {
+            Job curr = jobs.get(i);
+            if (curr.deadline > time) {
+                seq.add(curr.id);
+                time++;
+            }
+        }
+        System.out.println("Max jobs : " + seq.size());
+        for (int i = 0; i < seq.size(); i++) {
+            System.out.print(seq.get(i) + " ");
+        }
     }
 }
