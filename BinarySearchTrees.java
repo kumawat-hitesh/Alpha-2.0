a5e8a3edc631d869b663686af064d629ca905bce
Hitesh Kumawat <kumawathitesh2000@gmail.com>
2023-11-02 23:30:01 +0530

    BST part 2 complete

--- BinarySearchTrees.java
+++ BinarySearchTrees.java
@@ -6,26 +6,30 @@ public class BinarySearchTrees {
         Node left;
         Node right;
 
+        int height; // this is for AVL tree
+
         Node(int data) {
             this.data = data;
+            height = 1;
         }
     }
 
     // Insert in a binary search tree
-    public static Node insert(Node root, int val) { // TC : O(H) where H is height of tree and in worst case it is O(n)
-        if (root == null) {
-            root = new Node(val);
-            return root;
-        }
-        if (val < root.data) {
-            // left subtree
-            root.left = insert(root.left, val);
-        } else {
-            // right subtree
-            root.right = insert(root.right, val);
-        }
-        return root;
-    }
+    // public static Node insert(Node root, int val) { // TC : O(H) where H is
+    // height of tree and in worst case it is O(n)
+    // if (root == null) {
+    // root = new Node(val);
+    // return root;
+    // }
+    // if (val < root.data) {
+    // // left subtree
+    // root.left = insert(root.left, val);
+    // } else {
+    // // right subtree
+    // root.right = insert(root.right, val);
+    // }
+    // return root;
+    // }
 
     public static void inorder(Node root) {
         if (root == null) {
@@ -146,12 +150,211 @@ public class BinarySearchTrees {
         root.right = leftMirror;
         return root;
     }
-    public static void preorder(Node root){
-        if(root == null) return;
+
+    public static void preorder(Node root) {
+        if (root == null)
+            return;
 
         System.out.print(root.data + " ");
         preorder(root.left);
         preorder(root.right);
+    }
+
+    // Sorted array to BST
+    public static Node sortedArrayToBST(int[] arr, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        Node root = new Node(arr[mid]);
+        root.left = sortedArrayToBST(arr, start, mid - 1);
+        root.right = sortedArrayToBST(arr, mid + 1, end);
+        return root;
+    }
+
+    // BST to balanced BST
+    public static Node BSTToBalancedBST(Node root) {
+        // inorder sequence
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+        // sorted inorder -> balanced BST
+        root = createBST(inorder, 0, inorder.size() - 1);
+        return root;
+    }
+
+    public static void getInorder(Node root, ArrayList<Integer> inorder) {
+        if (root == null)
+            return;
+        getInorder(root.left, inorder);
+        inorder.add(root.data);
+        getInorder(root.right, inorder);
+    }
+
+    public static Node createBST(ArrayList<Integer> inorder, int start, int end) {
+        if (start > end)
+            return null;
+        int mid = (start + end) / 2;
+        Node root = new Node(inorder.get(mid));
+        root.left = createBST(inorder, start, mid - 1);
+        root.right = createBST(inorder, mid + 1, end);
+        return root;
+    }
+
+    // size of largest BST in BT
+    static class Info {
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+
+        public Info(boolean isBST, int size, int min, int max) {
+            this.isBST = isBST;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public static int maxBST = 0;
+
+    public static Info largestBST(Node root) {
+        if (root == null) {
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        Info leftInfo = largestBST(root.left);
+        Info rightInfo = largestBST(root.right);
+        int size = leftInfo.size + rightInfo.size + 1;
+        int min = Math.min(root.data, Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
+        if (root.data <= leftInfo.max || root.data >= rightInfo.min) {
+            return new Info(false, size, min, max);
+        }
+        if (leftInfo.isBST && rightInfo.isBST) {
+            maxBST = Math.max(maxBST, size);
+            return new Info(true, size, min, max);
+        }
+        return new Info(false, size, min, max);
+    }
+
+    // Merge 2 BST to form a single balanced BST
+    public static Node mergeBSTs(Node root1, Node root2) {
+        // step:1
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        getInorder(root1, arr1);
+
+        // step:2
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        getInorder(root2, arr2);
+
+        // merge
+        ArrayList<Integer> finalArr = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < arr1.size() && j < arr2.size()) {
+            if (arr1.get(i) <= arr2.get(j)) {
+                finalArr.add(arr1.get(i++));
+            } else {
+                finalArr.add(arr2.get(j++));
+            }
+        }
+        while (i < arr1.size()) {
+            finalArr.add(arr1.get(i++));
+        }
+        while (j < arr2.size()) {
+            finalArr.add(arr2.get(j++));
+        }
+
+        // sorted AL -> balanced BST
+        return createBST(finalArr, 0, finalArr.size() - 1);
+    }
+
+    // AVL tree
+    public static Node root;
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.height;
+    }
+
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public static int getBalanceFactor(Node root) {
+        if (root == null)
+            return 0;
+        return height(root.left) - height(root.right);
+    }
+
+    // left rotate subtree with node x
+    public static Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        // perform rotation
+        y.left = x;
+        x.right = T2;
+
+        // update heights
+        x.height = max(height(x.left), height(x.right) + 1);
+        y.height = max(height(y.left), height(y.right) + 1);
+
+        // return new root
+        return y;
+    }
+
+    // right rotate subtree with node x
+    public static Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        // perform rotation
+        x.right = y;
+        y.left = T2;
+
+        // update heights
+        y.height = max(height(y.left), height(y.right) + 1);
+        x.height = max(height(x.left), height(x.right) + 1);
+
+        // return new root
+        return x;
+    }
+
+    public static Node insert(Node root, int key) {
+        if (root == null) {
+            return new Node(key);
+        }
+        if (key < root.data) {
+            root.left = insert(root.left, key);
+        } else if (key > root.data) {
+            root.right = insert(root.right, key);
+        } else {
+            return root; // duplicate keys not allowed
+        }
+        // update root right
+        root.height = 1 + Math.max(height(root.left), height(root.right));
+        // get root balance factor
+        int balanceFactor = getBalanceFactor(root);
+        // left-left case
+        if (balanceFactor > 1 && key < root.left.data) {
+            return rightRotate(root);
+        }
+        // right-right case
+        if (balanceFactor < -1 && key > root.right.data) {
+            return leftRotate(root);
+        }
+        // left-right case
+        if (balanceFactor > 1 && key > root.left.data) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+        // right-left case
+        if (balanceFactor < -1 && key < root.right.data) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+        return root; // returned if AVL is balanced
     }
 
     public static void main(String[] args) {
@@ -187,23 +390,75 @@ public class BinarySearchTrees {
         // print root to leaf
         // printRoot2Leaf(root, new ArrayList<>());
 
-        //valid BST
+        // valid BST
         // if (isValidBST(root, null, null)) {
         // System.out.println("Valid");
         // } else {
         // System.out.println("Not valid");
         // }
 
-        //mirror BST
+        // mirror BST
         // Node root = new Node(8);
         // root.left = new Node(5);
         // root.right = new Node(10);
         // root.left.left = new Node(3);
         // root.left.right = new Node(6);
         // root.right.right = new Node(11);
-        
+
         // root = createMirrorBST(root);
         // preorder(root);
-        
+
+        // sorted array ot BST
+        // int arr[] = { 3, 5, 6, 8, 10, 11, 12 };
+        // Node root = sortedArrayToBST(arr, 0, arr.length-1);
+        // preorder(root);
+
+        // BST to balanced BST
+        // Node root = new Node(8);
+        // root.left = new Node(6);
+        // root.left.left = new Node(5);
+        // root.left.left.left = new Node(3);
+        // root.right = new Node(10);
+        // root.right.right = new Node(11);
+        // root.right.right.right = new Node(12);
+
+        // root = BSTToBalancedBST(root);
+        // preorder(root);
+
+        // size of largest BST in BT
+        // Node root = new Node(50);
+        // root.left = new Node(30);
+        // root.left.left = new Node(5);
+        // root.left.right = new Node(20);
+
+        // root.right = new Node(60);
+        // root.right.left = new Node(45);
+        // root.right.right = new Node(70);
+        // root.right.right.left = new Node(65);
+        // root.right.right.right = new Node(80);
+
+        // Info info = largestBST(root);
+        // System.out.println("Largest BST size : " + maxBST);
+
+        // Merge 2 BST to form a single balanced BST
+        // Node root1 = new Node(2);
+        // root1.left = new Node(1);
+        // root1.right = new Node(4);
+
+        // Node root2 = new Node(9);
+        // root2.left = new Node(3);
+        // root2.right = new Node(12);
+
+        // Node root = mergeBSTs(root1, root2);
+        // preorder(root);
+
+        // AVL tree
+        // root = insert(root, 10);
+        // root = insert(root, 20);
+        // root = insert(root, 30);
+        // root = insert(root, 40);
+        // root = insert(root, 50);
+        // root = insert(root, 25);
+        // preorder(root);
     }
 }
